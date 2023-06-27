d82a9ecb52ad4f126ddcac103ee3397a74c15d7f
Sören Poppe <soeren.poppe@gmx.de>
2023-06-27 15:47:30 +0200

    refactor directus classes

---
+++ app/src/main/java/de/haw_hamburg/playopolis/UserGames.java
@@ -0,0 +1,22 @@
+package de.haw_hamburg.playopolis;
+
+class UserGames {
+    private Integer users_id;
+    private Integer games_id;
+
+    public Integer getUsers_id() {
+        return users_id;
+    }
+
+    public void setUsers_id(Integer users_id) {
+        this.users_id = users_id;
+    }
+
+    public Integer getGames_id() {
+        return games_id;
+    }
+
+    public void setGames_id(Integer games_id) {
+        this.games_id = games_id;
+    }
+}
